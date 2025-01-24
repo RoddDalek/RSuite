8a3e352c6ef9239ce2ee79580f05be3221d8eebe
canetr1 <rodolfo.canetalbiach@aalto.fi>
2025-01-24 17:00:29 +0200

    Added a bit of functionality to files_util.py

--- scripts/files_util.py
+++ scripts/files_util.py
@@ -11,5 +11,24 @@ from tkinter.filedialog import askopenfilenames
 
 
 def ask_for_files():
+    """
+    Opens a tkinter Dialog to select some files.
+    :return: List with all the paths already normalized for all selected files.
+    """
     return [os.path.normpath(x) for x in askopenfilenames()]
+
+
+def import_data(filepath, separator='\t'):
+    """
+    In principle, it opens a .txt file and extracts the data.
+    :param filepath:
+    :param separator: Used separator in the file.
+    :return: A list with all data tuples extracted on the file.
+    """
+    aux = []
+    with open(filepath, 'r') as f:
+        data = f.readlines()
+    for item in data:
+        aux.append(item.split(separator))
+    return aux
 
---
+++ scripts/obsidian.py
@@ -0,0 +1,6 @@
+"""
+    obsidian.py
+    Author: Rodolfo Canet
+    Purpose: To hold functions that quickly alter one or many markdown files for Obsidian.
+"""
+
